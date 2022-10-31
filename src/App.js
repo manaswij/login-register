086dc127ea3acaccfc45664c1ad101af393906d0
manaswij <97697776+manaswij@users.noreply.github.com>
2022-10-31 08:08:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Homepage from "./components/homepage/homepage"
+import Login from "./components/login/login"
+import Register from "./components/register/register"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Homepage/> */}
+      <Login/>
+      {/* <Register/> */}
+    </div>
+  );
+}
+
+export default App;
